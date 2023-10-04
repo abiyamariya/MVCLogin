ea1563ae8e1403b6ca79f93e5a68886e1e75c40a
hp <hp@LAPTOP-M7T0CKNQ>
2023-10-04 12:34:32 +0530

    Add project files.

---
+++ MVCLogin/Controllers/LoginController.cs
@@ -0,0 +1,41 @@
+﻿using MVCLogin.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCLogin.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Authorize(MVCLogin.Models.User userModel)
+        {
+            using(LoginDataBaseEntities db=new LoginDataBaseEntities())
+            {
+                var userDetails = db.Users.Where(x => x.UserName == userModel.UserName && x.Password == userModel.Password).FirstOrDefault();
+                if(userDetails==null)
+                {
+                    userModel.LoginErrorMessage = "Wrong username or password";
+                    return View("Index", userModel);
+                }
+                else if(userDetails.UserID==1)
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index", "Customer");
+                }
+                else
+                {
+                    Session["userID"] = userDetails.UserID;
+                    return RedirectToAction("Index", "Member");
+                }
+            }
+        }
+    }
+}
---
+++ MVCLogin/Controllers/MemberController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCLogin.Models;
+
+namespace MVCLogin.Controllers
+{
+    public class MemberController : Controller
+    {
+        // GET: Member
+        public ActionResult Index()
+        {
+            using (DbModels dbModel = new DbModels())
+            {
+                return View(dbModel.Customers.ToList());
+            }
+        }
+    }
+}
